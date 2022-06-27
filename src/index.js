7dca398cc8d2a1b1cc7575363a6794822145a0f0
En1Li <1401207141@qq.com>
2022-06-27 20:53:25 +0800

    完成了插件和加载器等配置

--- src/index.js
+++ src/index.js
@@ -5,4 +5,20 @@
 import './banner.js'
 import './tabs.js'
 import $ from 'jquery'
+import '../src/styles/index.css'
+import './styles/index.less'
+import './assets/assets/fonts/iconfont.css'
 $('#swiper').css('background' , 'red')
+
+import imgUrl from '../src/assets/assets/1.gif';
+const img = document.createElement('img');
+img.src = imgUrl;
+document.body.appendChild(img);
+
+class App {
+    static a = 123
+  }
+  const fn = ()=>{
+    console.log(123);
+  }
+  console.log(App.a)
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
+    mode:'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -14,5 +15,43 @@ module.exports = {
         filename: 'index.html',
     }),
     new CleanWebpackPlugin()
-  ]
+  ],
+  devServer:{
+    open : true , 
+    port:3000
+  },
+  module:{
+    rules:[
+        {
+            test: /\.css$/,
+            use: [ "style-loader", "css-loader"]
+        },
+        {
+            test: /\.less$/,
+            use: [ "style-loader", "css-loader" , "less-loader"]
+        },
+        {
+            test: /\.(png|jpg|gif|jpeg)$/,
+        //    use: ['url-loader']
+            type:'asset'
+        },
+        {
+            test: /\.(eot|svg|ttf|woff|woff2)$/,
+            type: 'asset/resource',
+            generator: {
+    	    filename: 'font-[name].[hash:6][ext]'
+            }
+        },
+        {
+            test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+              loader: 'babel-loader',
+              options: {
+                  presets: ['@babel/preset-env'] // 预设:转码规则(用bable开发环境本来预设的)
+              }
+          }
+        }
+    ]
+  }
 };
